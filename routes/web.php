2045079b08ad972801e511078409ecbfdcc557e8
Xnyz-nizar <muhammad.nizar.ti.21@cic.ac.id>
2024-10-09 10:22:09 +0700

    booking

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Muhammad Nizar ']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
